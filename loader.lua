2a64e6329eeeead310a9b28dbc36bd28cda4ed2d
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-11 23:42:10 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,15 +1,9 @@
 local urlLoader = 'https://github.com/mrlthebest/Ragnarok-Bot/raw/refs/heads/main/ragnarokLoader.lua';
 do
     if not loadRemoteScript or type(loadRemoteScript) ~= 'function' then
-        HTTP.download(urlLoader , 'loader.lua', function(path, checksum, err)
-            if (err) then
-                warn(err);
-                return;
-            end
-            local content = g_resources.readFileContents('/downloads/' .. path);
-            loadstring(content)();
-            return;
-        end);
+        HTTP.get(urlLoader, function(script)
+            assert(loadstring(script))()
+        end)
     end
     loadRemoteScript(urlLoader);
 end
