81481077c0124f90b31fb2b442bb4ef3296c3c26
mrlthebest. <115442027+mrlthebest@users.noreply.github.com>
2025-01-29 03:44:13 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,5 @@
-local newVersion = 'https://raw.githubusercontent.com/mrlthebest/Ragnarok-Bot/refs/heads/main/ragnarokLoader20100.lua'; -- 20100 LuaJit
-local oldVersion = 'https://raw.githubusercontent.com/mrlthebest/Ragnarok-Bot/refs/heads/main/ragnarokLoader20005.lua'; -- 20005 LuaJit
-local urlLoader;
-if modules._G.jit.version_num < 20100 then urlLoader = oldVersion; else urlLoader = newVersion; end
-print(urlLoader .. ' - Version loaded');
+local urlLoader = 'https://raw.githubusercontent.com/mrlthebest/Ragnarok-Bot/refs/heads/main/ragnarokLoader.lua';
+
 do
     if not loadRemoteScript or type(loadRemoteScript) ~= 'function' then
         modules._G.HTTP.get(urlLoader, function(script)
